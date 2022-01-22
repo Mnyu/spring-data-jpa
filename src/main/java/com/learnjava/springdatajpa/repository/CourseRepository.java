301d978fccbbb2b33409ae7973a67237e1e15e30
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-01-22 14:35:54 +0530

    Paging and Sorting

--- src/main/java/com/learnjava/springdatajpa/repository/CourseRepository.java
+++ src/main/java/com/learnjava/springdatajpa/repository/CourseRepository.java
@@ -1,9 +1,13 @@
 package com.learnjava.springdatajpa.repository;
 
 import com.learnjava.springdatajpa.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Page<Course> findByCourseTitleContaining(String title, Pageable pageable);
 }
--- src/test/java/com/learnjava/springdatajpa/repository/CourseRepositoryTest.java
+++ src/test/java/com/learnjava/springdatajpa/repository/CourseRepositoryTest.java
@@ -5,6 +5,9 @@ import com.learnjava.springdatajpa.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -33,6 +36,41 @@ class CourseRepositoryTest {
                 .teacher(teacher)
                 .build();
         courseRepository.save(course);
+    }
+
+    @Test
+    public void findAllPagination() {
+        Pageable firstPageWith3Records = PageRequest.of(0, 3);
+        Pageable secondPageWith2Records = PageRequest.of(0, 2);
+
+        List<Course> courses = courseRepository.findAll(secondPageWith2Records).getContent();
+        long totalElements = courseRepository.findAll(secondPageWith2Records).getTotalElements();
+        int totalPages = courseRepository.findAll(secondPageWith2Records).getTotalPages();
+        System.out.println("Total Pages : " + totalPages);
+        System.out.println("Total Elements : " + totalElements);
+        System.out.println(courses);
+    }
+
+    @Test
+    public void findAllPaginationAndSort() {
+        Pageable page = PageRequest.of(0, 2, Sort.by("courseTitle"));
+        List<Course> courses = courseRepository.findAll(page).getContent();
+        System.out.println(courses);
+
+        Pageable page2 = PageRequest.of(0, 2, Sort.by("credit").descending());
+        courses = courseRepository.findAll(page2).getContent();
+        System.out.println(courses);
+
+        Pageable page3 = PageRequest.of(0, 2, Sort.by("courseTitle").descending().and(Sort.by("credit")));
+        courses = courseRepository.findAll(page3).getContent();
+        System.out.println(courses);
+    }
+
+    @Test
+    public void findByTitleContaining() {
+        Pageable firstPageWith10Records = PageRequest.of(0, 10);
+        List<Course> courses = courseRepository.findByCourseTitleContaining("D", firstPageWith10Records).getContent();
+        System.out.println(courses);
     }
 
 }
