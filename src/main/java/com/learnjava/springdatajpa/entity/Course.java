c208575013203c530fed2a141cb156fbad79d523
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2022-01-22 11:20:07 +0530

    Bi-directional mapping concept

--- src/main/java/com/learnjava/springdatajpa/entity/Course.java
+++ src/main/java/com/learnjava/springdatajpa/entity/Course.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 
 @Data
@@ -21,6 +22,14 @@ public class Course {
     @SequenceGenerator(name = "course_sequence", sequenceName = "course_sequence", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "course_sequence")
     private long courseId;
+
     private String courseTitle;
+
     private int credit;
+
+    // Bi-Directional OneToOne mapping
+    // The below means that one to one mapping is already defined at "course" field of CourseMaterial class.
+    @OneToOne(mappedBy = "course")
+    private CourseMaterial courseMaterial;
+
 }
